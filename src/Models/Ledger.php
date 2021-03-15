9c2fd6bd852feb8122d0449b04d99912c1511ec2
Rafal Cegielka <rafalcegielka@motogb.co.uk>
2021-03-15 21:18:45 +0000

    u

--- src/Models/Ledger.php
+++ src/Models/Ledger.php
@@ -77,20 +77,7 @@ class Ledger extends Model implements Segregatable
      */
     private static function postVat(LineItem $lineItem, Transaction $transaction): void
     {
-        
-        if($lineItem->vat_inclusive === 'true'){
-            $amount = $lineItem->amount - ($lineItem->amount / (1 + ($lineItem->vat->rate / 100)));
-        }
-
-        if($lineItem->vat_inclusive === 'false'){
-            $amount = $lineItem->amount * $lineItem->vat->rate / 100;
-        }
-
-        if($lineItem->vat_inclusive === 'margin'){
-            if(isset($lineItem->vat_amount)){
-                $amount = $lineItem->vat_amount;
-            }
-        }
+        $amount = $lineItem->vat_inclusive ?  $lineItem->amount - ($lineItem->amount / (1 + ($lineItem->vat->rate / 100))) : $lineItem->amount * $lineItem->vat->rate / 100;
 
         $post = new Ledger();
         $folio = new Ledger();
--- src/Models/Transaction.php
+++ src/Models/Transaction.php
@@ -10,7 +10,6 @@
 
 namespace IFRS\Models;
 
-use Response;
 use Carbon\Carbon;
 
 use Illuminate\Support\Facades\Auth;
@@ -423,28 +422,13 @@ class Transaction extends Model implements Segregatable, Recyclable, Clearable, 
                 $amount += $ledger->amount / $this->exchangeRate->rate;
             }
         } else {
-
             foreach ($this->getLineItems() as $lineItem) {
                 $amount += $lineItem->amount * $lineItem->quantity;
-                if ($lineItem->vat_inclusive === 'false') {
+                if (!$lineItem->vat_inclusive) {
                     $amount += $lineItem->amount * ($lineItem->vat->rate / 100) * $lineItem->quantity;
                 }
-                if ($lineItem->vat_inclusive === 'margin') {
-                    if(isset($lineItem->vat_amount)){
-                        $amount += $lineItem->vat_amount;
-                    }else{
-                        $amount += 0;
-                    }
-                }
-                return Response::json([
-                    'status' => 'success',
-                    'amount' => $amount,
-                    'st' => $lineItem->vat_inclusive
-                ], 200);
             }
-            
         }
-        
         return $amount;
     }
 
